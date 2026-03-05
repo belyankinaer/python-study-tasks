d45f9af7f2087c2298af5ba979377d88f1bb8b86
eli.b <124588751+belyankinaer@users.noreply.github.com>
2026-03-05 15:08:53 +0300

    Create ad_rewriter.py

---
+++ data_types/string/ad_rewriter.py
@@ -0,0 +1,5 @@
+advertising_text = 'Не пропустите наше ограниченное предложение на новые модели!'
+old_text = 'ограниченное'
+new_text = 'эксклюзивное'
+rewritten_advertising_text = advertising_text.replace(old_text, new_text)
+print(rewritten_advertising_text)
