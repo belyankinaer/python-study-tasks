1c13a8b66124d20b560e2a861ed73e2ea42e467f
eli.b <124588751+belyankinaer@users.noreply.github.com>
2026-03-05 14:55:04 +0300

    Create variables_processing.py

---
+++ variables/variables_processing.py
@@ -0,0 +1,136 @@
+# hw variable
+
+# task 1 - int
+
+books_count = 120
+print(books_count)
+
+books_count = 135
+print(books_count)
+
+# task 2 - int
+
+students_in_class_count = 23
+print(students_in_class_count)
+
+students_in_class_count = 25
+print(students_in_class_count)
+
+# task 3 - float
+
+air_temperature = 24.7
+print(air_temperature)
+
+air_temperature = 18.2
+print(air_temperature)
+
+# task 4 - float
+
+test_score = 4.5
+print(test_score)
+
+test_score = 4.8
+print(test_score)
+
+# task 5 - str
+
+favorite_quote = "Hello World!"
+print(favorite_quote)
+
+favorite_quote = "Hello!"
+print(favorite_quote)
+
+# task 6 - str
+
+full_name = "Belyankina Elizaveta"
+print(full_name)
+
+full_name = "Belyankina Ekaterina"
+print(full_name)
+
+# task 7 - bool
+
+is_raining = True
+print(is_raining)
+
+is_raining = False
+print(is_raining)
+
+# task 8 - bool
+
+is_door_open = False
+print(is_door_open)
+
+is_door_open = True
+print(is_door_open)
+
+# hw type conversion
+
+# task 1
+
+bananas_per_apple = 2.0
+
+num_apple = 15
+num_banana = 15 * bananas_per_apple
+print(f"Количество яблок: {num_apple}, количество бананов: {num_banana}")
+
+# task 2
+
+first_str = "world"
+second_str = "hello"
+result_str = second_str + " " + first_str
+print(result_str)
+
+# task 3
+
+num_apple = "256"
+num_banana = 44
+
+total_num_fruit = int(num_apple) + num_banana
+print(total_num_fruit)
+
+# task 4
+
+is_raining = True
+is_sunny = False
+
+result_and = is_raining and is_sunny
+print(result_and)
+
+result_or = is_raining or is_sunny
+print(result_or)
+
+result_not = not is_raining and not is_sunny
+print(result_not)
+
+# task 5
+
+cost_coffee = 500.99
+cost_coffee = str(cost_coffee)
+print(cost_coffee)
+
+# task 6
+
+num_apple = 8
+num_sugar = 2.18
+
+num_apple = str(num_apple)
+num_sugar = str(num_sugar)
+
+print(num_apple + num_sugar)
+
+# task 7
+
+num_apple = 9
+num_sugar = 4.5
+
+print(num_apple + num_sugar)
+
+# task 8
+
+cost_bus = "20"
+cost_bus = int(cost_bus)
+
+cost_metro = 30
+
+print(cost_bus + cost_metro)
